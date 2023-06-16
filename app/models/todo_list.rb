cc2e0ebb66dd3304a11a6545381388535c0334d7
Dayanne Bugarim <dayannebugarim1@gmail.com>
2023-06-16 13:54:49 -0300

    :sparkles: feat: adding new colors to the model (#5)

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -2,7 +2,7 @@ class TodoList < ApplicationRecord
     has_many :list_items, dependent: :destroy
 
     validates :title, presence: true
-    validates :color, allow_blank: true, inclusion: { in: ["#5E93E3", "#E35E5E", "#5EE393"], allow_nil: true }
+    validates :color, allow_blank: true, inclusion: { in: ["#5E93E3", "#E35E5E", "#5EE393", "#E37ACC", "#EDCE7E", "#8D91F4"], allow_nil: true }
     validates :done, inclusion: { in: [true, false] }
   
     before_validation :set_defaults
