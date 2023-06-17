700458f2555ceb1c2b343f63c9a014c8323d5f81
Dayanne Bugarim <dayannebugarim1@gmail.com>
2023-06-16 21:02:42 -0300

    :recycle: refactor: simplifying form views code (#5)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    
+    def render_errors(object)
+        if object.errors.any?
+          content_tag :div, id: "error_explanation" do
+            concat content_tag(:h2, "#{pluralize(object.errors.count, "error")} prohibited this #{object.class.name.downcase} from being saved:")
+            concat(content_tag(:ul) do
+              object.errors.full_messages.each do |message|
+                concat content_tag(:li, message)
+              end
+            end)
+          end
+        end
+    end
 end
